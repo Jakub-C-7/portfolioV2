696eb243253db96f115287e31736c43861af43e0
Jakub-C-7 <jakubchamera7@gmail.com>
2023-05-16 22:50:56 +0100

    Added 3d models and converted to jsx

--- app/components/canvas/CampCanvas.jsx
+++ app/components/canvas/CampCanvas.jsx
@@ -1,7 +1,54 @@
-import React, { Component } from "react";
+"use client";
 
-export default class CampCanvas extends Component {
-  render() {
-    return <div>CampCanvas</div>;
-  }
-}
+import { Suspense, useEffect, useState } from "react";
+import { Canvas, useLoader } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
+import { Camp } from "../Camp";
+
+const CampCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  // Check if the user is on a mobile device dependent on screen width
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the mediaQuery variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      // Remove the listener when the component unmounts
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+  return (
+    <Canvas
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Camp isMobile={isMobile} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default CampCanvas;
