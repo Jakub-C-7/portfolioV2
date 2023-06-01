f94e771e698e44abc38a0e9f9c6d782200622f75
Jakub-C-7 <jakubchamera7@gmail.com>
2023-06-01 07:53:19 +0100

    Added framer motion to area

--- app/components/WorkshopGreeting.jsx
+++ app/components/WorkshopGreeting.jsx
@@ -2,21 +2,23 @@
 
 import { motion } from "framer-motion";
 import { styles } from "../styles";
+import { fadeIn, textVariant } from "../utils/motion";
 import WorkbenchCanvas from "./canvas/WorkbenchCanvas";
 
 const WorkshopGreeting = () => {
   return (
     <section id="workshop" className="relative w-full h-screen mx-auto">
-      <div
+      <motion.div
+        variants={textVariant()}
         className={`${styles.paddingX} max-w-7xl mx-auto flex flex-row items-end gap-5`}
       >
-        <div className="flex flex-col">
+        <motion.div variants={fadeIn("", "", 0.1, 1)} className="flex flex-col">
           <h1 className={`${styles.headText} `}>the workshop</h1>
           <h2 className={`${styles.subTextAlt2} pl-10 text-white`}>
             where projects are born
           </h2>
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
 
       <WorkbenchCanvas />
     </section>
