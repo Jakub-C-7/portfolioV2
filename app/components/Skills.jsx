70b76fdaeaab8ac7571493e16d7473320e77fa59
Jakub-C-7 <jakubchamera7@gmail.com>
2023-06-23 18:15:54 +0100

    added my own tooltip and made it scale up/down

--- app/components/Skills.jsx
+++ app/components/Skills.jsx
@@ -6,23 +6,25 @@ import { technologies } from "../constants";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { textVariant } from "../utils/motion";
-import { Tooltip } from "@nextui-org/react";
 
 const Skills = () => {
   return (
     <ComponentLayout>
       <motion.div variants={textVariant()} id="tech">
-        <p className={styles.sectionSubText}>the tools in the workshop</p>
+        <p className={styles.sectionSubText}>recent tools in the workshop</p>
         <h2 className={styles.sectionHeadText}>technologies</h2>
       </motion.div>
 
-      <div className="flex flex-row flex-wrap justify-center gap-10 sm:mt-8">
+      <div className="flex flex-wrap justify-center gap-10 mt-8 w-3/4 mx-auto">
         {technologies.map((technology) => (
-          <div className="sm:w-1/4 sm:h-1/4 w-1/3 h-1/3" key={technology.name}>
-            <Tooltip content={`${technology.name}`}>
-              <FloppyCanvas icon={technology.icon} />
-            </Tooltip>
-            {/* {technology.name} */}
+          <div
+            className="relative sm:w-1/4 sm:h-1/4 w-1/3 h-1/3"
+            key={technology.name}
+          >
+            <FloppyCanvas icon={technology.icon} />
+            <motion.div className="absolute select-none active:pointer-events-none text-tertiary top-0 left-0 w-full h-1/2 text-center opacity-0 transition duration-700 hover:opacity-100 hover:scale-110">
+              {technology.name}
+            </motion.div>
           </div>
         ))}
       </div>
