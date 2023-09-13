7d5ff6b7bf22e04e5a72f9f8be9486577e8aedf5
Jakub-C-7 <jakubchamera7@gmail.com>
2023-09-13 17:26:54 +0100

    tried work around for dynamically removing and readding canvases

--- app/components/Skills.jsx
+++ app/components/Skills.jsx
@@ -7,8 +7,48 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { textVariant } from "../utils/motion";
 import { Canvas, useLoader } from "@react-three/fiber";
+import { Html } from "@react-three/drei";
+import React from "react";
+import { useInView } from "react-intersection-observer";
 
 const Skills = () => {
+  const [ref, inView] = useInView({
+    /* Optional options */
+    triggerOnce: false,
+    rootMargin: "0px 0px",
+  });
+
+  let canvasDisplay;
+
+  if (inView) {
+    {
+      canvasDisplay = technologies.map((technology) => (
+        <div
+          className="relative sm:w-1/4 sm:h-1/4 w-1/3 h-1/3"
+          key={technology.name}
+        >
+          <Canvas
+            key={technology.name}
+            className="cursor-pointer"
+            frameloop="always" //Always animates all floppy disks
+            shadows
+            camera={{
+              position: [0, -0.5, 4],
+              fov: 50,
+              rotation: [0, 0, 0],
+              near: 0.1,
+            }}
+            gl={{ preserveDrawingBuffer: true }}
+          >
+            <FloppyCanvas icon={technology.icon} />
+          </Canvas>
+        </div>
+      ));
+    }
+  } else {
+    canvasDisplay = "";
+  }
+
   return (
     <ComponentLayout>
       <motion.div
@@ -38,31 +78,11 @@ const Skills = () => {
         ))}
       </div> */}
 
-      <div className="flex flex-wrap justify-center mt-8 w-3/4 mx-auto">
-        {technologies.map((technology) => (
-          <div
-            className="relative sm:w-1/4 sm:h-1/4 w-1/3 h-1/3"
-            key={technology.name}
-          >
-            <Canvas
-              className="cursor-pointer"
-              frameloop="always" //Always animates all floppy disks
-              shadows
-              camera={{
-                position: [0, -0.5, 4],
-                fov: 50,
-                rotation: [0, 0, 0],
-                near: 0.1,
-              }}
-              gl={{ preserveDrawingBuffer: false }}
-            >
-              <FloppyCanvas icon={technology.icon} />
-              {/* <motion.div className="absolute select-none active:pointer-events-none text-tertiary top-0 left-0 w-full h-2/3 text-center opacity-0 transition duration-700 hover:opacity-100 hover:scale-110">
-                {technology.name}
-              </motion.div> */}
-            </Canvas>
-          </div>
-        ))}
+      <div
+        ref={ref}
+        className="flex flex-wrap justify-center mt-8 w-3/4 mx-auto h-screen"
+      >
+        {canvasDisplay}
       </div>
     </ComponentLayout>
   );
