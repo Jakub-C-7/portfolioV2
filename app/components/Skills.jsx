64b03bcf266f911a312c9cadde0e881fed6b2846
Jakub-C-7 <jakubchamera7@gmail.com>
2023-09-13 14:09:39 +0100

    added all floppy disks to one canvas for testing

--- app/components/Skills.jsx
+++ app/components/Skills.jsx
@@ -6,6 +6,7 @@ import { technologies } from "../constants";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { textVariant } from "../utils/motion";
+import { Canvas, useLoader } from "@react-three/fiber";
 
 const Skills = () => {
   return (
@@ -36,6 +37,33 @@ const Skills = () => {
           </div>
         ))}
       </div> */}
+
+      <div className="flex flex-wrap justify-center gap-5 mt-8 w-3/4 mx-auto">
+        {technologies.map((technology) => (
+          <div
+            className="relative sm:w-1/4 sm:h-1/4 w-1/3 h-1/3"
+            key={technology.name}
+          >
+            <Canvas
+              className="cursor-pointer"
+              frameloop="always" //Always animates all floppy disks
+              shadows
+              camera={{
+                position: [0, -0.5, 4],
+                fov: 50,
+                rotation: [0, 0, 0],
+                near: 0.1,
+              }}
+              gl={{ preserveDrawingBuffer: true }}
+            >
+              <FloppyCanvas icon={technology.icon} />
+              {/* <motion.div className="absolute select-none active:pointer-events-none text-tertiary top-0 left-0 w-full h-2/3 text-center opacity-0 transition duration-700 hover:opacity-100 hover:scale-110">
+                {technology.name}
+              </motion.div> */}
+            </Canvas>
+          </div>
+        ))}
+      </div>
     </ComponentLayout>
   );
 };
